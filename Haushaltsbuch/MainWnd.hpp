496158a82da033e855d65f34257cab2d6ee24e17
Sakura-yukikaze <sakura_yukikaze@live.jp>
2018-01-24 22:58:40 +0900

    Bump version Rev.5.

--- Haushaltsbuch/MainWnd.hpp
+++ Haushaltsbuch/MainWnd.hpp
@@ -3,7 +3,7 @@
 #include "MinimalIniFile.hpp"
 
 #define WINDOW_CLASS _T("Haushaltsbuch")
-#define WINDOW_TEXT  _T("‰ØŒv•ë Rev.4")
+#define WINDOW_TEXT  _T("‰ØŒv•ë Rev.5")
 
 bool MainWindow_Initialize();
 bool MainWindow_PreTranslateMessage(MSG &msg);
